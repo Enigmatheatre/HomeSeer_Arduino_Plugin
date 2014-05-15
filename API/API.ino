4684dd3f6227f962d8efba1c4a56e14d220f286c
Enigmatheatre <greigdempster@hotmail.com>
2014-05-15 17:44:55 +0100

    API.ino Added.
    This is a test to check the Sync. This is not a working ino it is a work
    in progress.

---
+++ API/API.ino
@@ -0,0 +1,283 @@
+
+#include <SPI.h>       
+#include <Ethernet.h>
+#include <EthernetUdp.h> 
+/************************************************************
+ *Arduino to Homeseer 3 Plugin API writen by Enigma Theatre.*
+ * V1.0.0.22                                                *
+ *                                                          *
+ *******Change the values below only*************************
+ */
+
+
+//Address of the board.
+byte BoardAdd = 1;
+
+//For serial set to 0 and for Ethernet set to 1
+#define ISIP 0
+
+// Enter a MAC address and IP address for your board below.
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+// The IP address will be dependent on your local network.
+IPAddress ip(192,168,0,100);     //IP entered in HS config.
+
+unsigned int localPort = 9000;      //port entered in HS config.
+
+IPAddress HomeseerIP(192,168,0,123); //Homeseer IP address
+
+
+int FromHS[10];
+
+//************Do not change anything in Here*****************
+// *
+boolean IsConnected = false;                                // *
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];               // *
+EthernetUDP Udp;                                         // *
+unsigned int ServerPort = 8888;                          // *
+//***********************************************************
+
+
+void setup() {
+  HSSetup();
+  //************************
+  //Add YOUR SETUP HERE;
+  //************************
+
+
+
+
+}
+
+
+
+
+void loop() {
+  IsUDP();
+  if (IsConnected == true)
+  {
+
+
+    //************************
+    //Add YOUR CODE HERE;
+    //************************
+    /* To Send Data to Homeseer use SendToHS(Device,Value)
+     Eg.. SendToHS(1,200); where 1 is the API device in homeseer and 200 is the value to send
+     To Recieve data from Homeseer look up the FromHS array that is updated when the device value changes.
+     Eg.. FromHS[5] would be the data from API Output device 5
+     */
+
+
+
+
+
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char* Version = "API1.0.0.22";
+
+byte Byte1,Byte2,Byte3;
+int Byte4,Byte5;
+
+
+void HSSetup() {
+
+#if ISIP == 1
+  Ethernet.begin(mac,ip);
+  Udp.begin(localPort);
+  Udp.setTimeout(0);
+  delay(1000);
+  Udp.beginPacket(HomeseerIP, ServerPort);
+  Udp.print("Connect ");
+  Udp.println(BoardAdd);
+  Udp.endPacket();
+#else
+  Serial.begin(115200);
+  Serial.flush();
+  Serial.setTimeout(0);
+  delay(1000);
+  Serial.print("Connect ");
+  Serial.println(BoardAdd);
+#endif
+
+  IsConnected = false;
+
+}
+
+void IsUDP(){
+  int packetSize = Udp.parsePacket();
+  if(packetSize)
+  {
+    IPAddress remote = Udp.remoteIP();
+    Byte1 =Udp.parseInt();
+    Udp.read(); 
+    Byte2 =Udp.read(); 
+    Byte3 =Udp.parseInt();
+    Byte4 =Udp.parseInt();
+    Byte5 =Udp.parseInt();
+    DataEvent();
+  }
+}
+
+
+void serialEvent() {
+  while (Serial.available() > 0) {
+    delay(17);
+
+
+    Byte1 = Serial.parseInt();
+    Serial.read();  
+    Byte2 = Serial.read(); 
+    Byte3 = Serial.parseInt();
+    Byte4 = Serial.parseInt();
+    Byte5 = Serial.parseInt();
+    DataEvent();
+  }
+}
+
+void DataEvent() {
+
+
+
+  if (Byte1 == BoardAdd) {
+    switch (Byte2) {
+
+    case 'c':
+      IsConnected = true;
+      break;
+
+    case 'C':   
+#if ISIP == 1
+      Udp.beginPacket(Udp.remoteIP(), ServerPort);
+      Udp.print("Version ");
+      Udp.print(BoardAdd);
+      Udp.print(" ");
+      Udp.print(Version);
+      Udp.println(" HS3");
+      Udp.endPacket();
+
+      Udp.beginPacket(Udp.remoteIP(), ServerPort);
+      delay(100);
+      Udp.print("Connected ");
+      Udp.println(BoardAdd);
+      Udp.endPacket();
+#else
+      Serial.print("Version ");
+      Serial.print(BoardAdd);
+      Serial.print(" ");
+      Serial.print(Version);
+      Serial.println(" HS3"); 
+      delay(100);
+      Serial.print("Connected ");
+      Serial.println(BoardAdd);
+#endif
+      delay(100);
+      IsConnected = false;
+      break;
+
+    case 'K':
+      delay(200);
+#if ISIP == 1
+      Udp.beginPacket(Udp.remoteIP(), ServerPort);
+      Udp.print("Alive ");
+      Udp.println(BoardAdd);
+      Udp.endPacket();
+#else     
+      Serial.print("Alive ");
+      Serial.println(BoardAdd);
+#endif
+      break; 
+
+    case 'O':
+      FromHS[Byte3] = Byte4;
+      break; 
+
+    case 'D':
+      IsConnected = false;
+      break;   
+    }
+  }
+}
+
+void SendToHS(byte Device, long Data){
+
+#if ISIP == 1
+  Udp.beginPacket(Udp.remoteIP(), ServerPort);
+  Udp.print(BoardAdd);
+  Udp.print(" API ");
+  Udp.print(Device);
+  Udp.print(" ");
+  Udp.print(Data);
+  Udp.endPacket();
+#else
+  Serial.print(BoardAdd);
+  Serial.print(" API ");
+  Serial.print(Device);
+  Serial.print(" ");
+  Serial.println(Data);
+#endif
+}
+
+
+
